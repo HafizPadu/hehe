431a0a0826e74345b35f0860436da146e040a972
HafizPadu <hafizshahili4@gmail.com>
2025-05-19 10:23:27 +0800

    Delete loaner_system directory

--- loaner_system/filter_form.php
+++
@@ -1,9 +0,0 @@
-<form method="GET" id="filter-form">
-    <input type="text" name="search" placeholder="Search item, loaner, or location" value="<?php echo htmlspecialchars($search); ?>" />
-    <select name="status">
-        <option value="">All Status</option>
-        <option value="loaned" <?php if ($filter_status === 'loaned') echo 'selected'; ?>>Loaned</option>
-        <option value="returned" <?php if ($filter_status === 'returned') echo 'selected'; ?>>Returned</option>
-    </select>
-    <button type="submit">Filter</button>
-</form>
